1e0b055c7423fc4f48d2342cc21af3df8126273d
saish0 <patrickvalera50@yahoo.com>
2021-12-23 05:07:48 -0800

    landing page add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
 import "./App.css";
-import {  ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 // COMPONENT IMPORTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import QueryScreen from "./screens/QueryScreen";
 import RecipeScreen from "./screens/RecipeScreen";
 import LandingScreen from "./screens/LandingScreen";
-import {defaultTheme} from './themes/defaultTheme'
+import { defaultTheme } from './themes/defaultTheme'
 function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<div className='App'>
 
 				<Router>
-				<Header />
 					<Routes>
 						<Route path='/' element={<LandingScreen />} />
-						<Route path='search/*' element={<QueryScreen />} />
-						<Route path='/recipe/:id' element={<RecipeScreen />} />
+						<Route path='/main' element={<><Header/><Outlet/><Footer/></>}>
+							<Route path='search/*' element={<QueryScreen />} />
+							<Route path='recipe/:id' element={<RecipeScreen />} />
+						</Route>
 					</Routes>
 				</Router>
 
-				<Footer />
-			</div>
 		</ThemeProvider>
 	);
 }
--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const LandingScreen = () => {
-	return <div>Landing Page</div>;
+	let navigate=useNavigate()
+	return (
+	<div className='landing-page'>
+		<button className='start-button' onClick={()=>navigate('main/search')}>GET STARTED</button>
+		<div className='overlay'/>
+		<img className='landing-page-image' src={'https://images.pexels.com/photos/604969/pexels-photo-604969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} alt='food-background'/>
+	</div>)
 };
 
 export default LandingScreen;
